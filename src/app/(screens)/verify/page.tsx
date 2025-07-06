47fb8a9a9f045dd518ee5eca243c5dc162624460
Manish Suthar <manishsuthar078@gmail.com>
2025-07-06 20:14:31 +0530

    fix(build): suspense boundary wrapped verify component

--- src/app/(screens)/verify/page.tsx
+++ src/app/(screens)/verify/page.tsx
@@ -1,31 +1,11 @@
-"use client";
-import { instance } from "@/lib/axios.instance";
-import { Loader2 } from "lucide-react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import Verify from "@/components/Verify";
+import { Suspense } from "react";
+import Loading from "@/components/shared/loading";
 
-const Page = () => {
-  const token = useSearchParams().get("token");
-  const router = useRouter();
-
-  useEffect(() => {
-    const verifyAccount = async () => {
-      if (!token) return;
-      await instance.get(`/verify?token=${token}`);
-      router.replace("/");
-    };
-    verifyAccount();
-  }, [router]);
+export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center">
-        <div className="my-2">
-          <Loader2 className="animate-spin" />
-        </div>
-        <p>Please be patient, while we are verifying your account.</p>
-      </div>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Verify />
+    </Suspense>
   );
-};
-
-export default Page;
+}
