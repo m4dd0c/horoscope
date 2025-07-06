badf92e773ab2cca44ffc3c6d4f531ad09d4e9cb
Manish Suthar <manishsuthar078@gmail.com>
2025-07-06 14:31:55 +0530

    feat: axios.instance created with response interceptor

---
+++ src/lib/axios.instance.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { toast } from "sonner";
+export const instance = axios.create({
+  baseURL: "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+instance.interceptors.response.use(
+  (res) => {
+    if (res?.data?.message) {
+      if (res?.data?.success) toast.success(res?.data?.message);
+      else toast.error(res?.data?.message);
+    }
+    return res;
+  },
+  (error): Promise<string> => {
+    // Any status codes outside the range of 2xx will trigger this function
+    if (axios.isAxiosError(error)) {
+      // checking for specific error codes or responses here
+      console.error(
+        "Axios error occurred:",
+        error?.response?.data?.message || error?.message,
+      );
+      toast.error(error?.response?.data?.message || error?.message);
+    } else {
+      console.error("Unexpected error occurred:", error?.message);
+      toast.error(error?.message);
+    }
+    // Optionally, return a rejected promise to propagate the error further
+    return Promise.reject(error);
+  },
+);
