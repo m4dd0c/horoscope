c587d7650cde52a775a66740b65dfe49f7410536
Manish Suthar <manishsuthar078@gmail.com>
2025-07-06 14:33:19 +0530

    chore: constants file created

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const logo =
+  "https://media.istockphoto.com/id/466539386/vector/hand-drawn-pentagram-icon-scanned-and-vectorized-brush-drawing.jpg?s=612x612&w=0&k=20&c=8Onemy7j1Sz8JT5m9_2XarmJjTmVBJUhYilvvYn3Fy0=";
+export const hero =
+  "https://thumbs.dreamstime.com/b/night-landscape-colorful-milky-way-yellow-light-mountains-starry-sky-hills-summer-beautiful-universe-space-72956059.jpg?w=992";
+
+export const horoscopeApiUrl =
+  "https://horoscope-app-api.vercel.app/api/v1/get-horoscope";
