4c1ee537efb7a1a3e70d6fe3e661604c18369df5
Manish Suthar <manishsuthar078@gmail.com>
2025-07-06 14:34:46 +0530

    feat(protected): protected route created for /dashboard page

---
+++ src/components/withAuth.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Loading from "@/components/shared/loading";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { instance } from "@/lib/axios.instance";
+
+const withAuth = (WrappedComponent: any) => {
+  const ComponentWithAuth = (props: any) => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchUser = async () => {
+        try {
+          const { data } = await instance.get("/user");
+
+          if (!data?.user) {
+            router.replace("/signin");
+          }
+        } catch (err) {
+          router.replace("/signin");
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchUser();
+    }, [router]);
+
+    if (loading) return <Loading />;
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return ComponentWithAuth;
+};
+
+export default withAuth;
